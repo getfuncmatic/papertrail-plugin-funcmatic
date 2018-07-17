d5a9df74c581b48498f09309cab07ed71d4ddb07
Daniel Jhin Yoo <danieljyoo@gmail.com>
2018-07-16 22:02:56 -0700

    added level option and consoleOnly to not send to paperclip for testing dev purposes

--- test/papertrail.test.js
+++ test/papertrail.test.js
@@ -2,7 +2,13 @@ var Funcmatic = require('@funcmatic/funcmatic')
 var PapertrailPlugin = require('../lib/papertrail')
 
 var handler = Funcmatic.wrap(async (event, { papertrail }) => {
-  papertrail.info('this is my message')
+  console.log("consoleOnly", papertrail.consoleOnly) 
+  var message = 'this is my message to papertrail'
+  if (papertrail.consoleOnly) {
+    message = 'this is my message to console only'
+  } 
+  papertrail.info(message)
+
   return {
     statusCode: 200
   }
@@ -21,5 +27,15 @@ describe('Request', () => {
     var context = { }
     var ret = await handler(event, context)
     console.log("RET", ret)
+  })
+  it ('should not send a papertrail log event', async () => {
+    Funcmatic.use(PapertrailPlugin, { 
+      host: process.env.PAPERTRAIL_HOST,
+      port: process.env.PAPERTRAIL_PORT,
+      consoleOnly: true
+    })
+    var event = { }
+    var context = { }
+    var ret = await handler(event, context)
   })
 })
